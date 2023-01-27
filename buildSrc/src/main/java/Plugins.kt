456c14ba5c384dd028e0ca98fdc9bd2d65641482
junsuPark <krpjs0508@gmail.com>
2023-01-27 21:42:22 +0900

    ✨ :: buildSrc 버전 및 의존성 클래스 선언

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,26 @@
+object Plugins {
+    object Android {
+        const val ANDROID_APPLICATION = "com.android.application"
+        const val ANDROID_LIBRARY = "com.android.library"
+    }
+
+    object Kotlin {
+        const val KOTLIN = "org.jetbrains.kotlin.android"
+        const val KOTLIN_ANDROID = "kotlin-android"
+        const val KOTLIN_KAPT = "kotlin-kapt"
+        const val KTLINT = "org.jlleitschuh.gradle.ktlint"
+    }
+
+    object DI {
+        const val HILT_ANDROID = "dagger.hilt.android.plugin"
+    }
+
+    object Gradle {
+        const val GRADLE_ANDROID =
+            "com.android.tools.build:gradle:${Versions.Gradle.GRADLE_ANDROID_VERSION}"
+        const val GRADLE_KOTLIN =
+            "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.Gradle.GRADLE_KOTLIN_VERSION}"
+        const val GRADLE_HILT_ANDROID =
+            "com.google.dagger:hilt-android-gradle-plugin:${Versions.Gradle.GRADLE_HILT_ANDROID_VERSION}"
+    }
+}
