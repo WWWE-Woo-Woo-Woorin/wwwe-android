9c6d353f8f0a0b8148d5f1af8d7ff9a10090f15f
junsuPark <krpjs0508@gmail.com>
2023-01-31 18:12:18 +0900

    ✨ :: implement HTTPHandler

---
+++ data/src/main/java/app/junsu/data/util/HTTPHandler.kt
@@ -0,0 +1,37 @@
+package app.junsu.data.util
+
+import app.junsu.domain.status.client.ClientStatus.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.net.UnknownHostException
+
+object HTTPHandler {
+    suspend inline operator fun <T> invoke(
+        crossinline block: suspend () -> T,
+    ): T {
+        return try {
+            withContext(Dispatchers.IO) {
+                block()
+            }
+        } catch (e: HttpException) {
+            e.handleHttpException()
+        } catch (e: UnknownHostException) {
+            throw NoInternet()
+        }
+    }
+
+    @PublishedApi
+    internal fun HttpException.handleHttpException(): Nothing {
+        throw when (code()) {
+            400 -> BadRequest()
+            401 -> Unauthorized()
+            403 -> Forbidden()
+            404 -> NotFound()
+            405 -> MethodNotAllowed()
+            409 -> Conflict()
+            429 -> TooManyRequests()
+            else -> Unknown()
+        }
+    }
+}
