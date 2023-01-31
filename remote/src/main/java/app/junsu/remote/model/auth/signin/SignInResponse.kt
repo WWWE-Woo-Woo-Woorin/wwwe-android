734925f11c42c98bf3f98f70d72f4827f9bd7fa3
junsuPark <krpjs0508@gmail.com>
2023-01-31 19:13:32 +0900

    ✨ :: implement toToken() in SigninResponse

--- remote/src/main/java/app/junsu/remote/model/auth/signin/SignInResponse.kt
+++ remote/src/main/java/app/junsu/remote/model/auth/signin/SignInResponse.kt
@@ -1,10 +1,19 @@
 package app.junsu.remote.model.auth.signin
 
+import app.junsu.model.Token
 import com.google.gson.annotations.SerializedName
 import java.time.LocalDateTime
 
 data class SignInResponse(
-    @SerializedName("refresh_token") val refreshToken: String,
     @SerializedName("access_token") val accessToken: String,
-    @SerializedName("refresh_token_exp") val refreshTokenExpiresAt: LocalDateTime,
-)
+    @SerializedName("refresh_token") val refreshToken: String,
+    @SerializedName("refresh_token_exp") val accessTokenExpiresAt: LocalDateTime,
+) {
+    fun toToken(): Token {
+        return Token(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+            accessTokenExpiresAt = accessTokenExpiresAt,
+        )
+    }
+}
