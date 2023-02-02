a4ab3c06d64422308cb7ae83947d591dc638e959
junsuPark <krpjs0508@gmail.com>
2023-02-02 20:22:58 +0900

    ✨ :: add MainActivity

---
+++ app/src/main/java/app/junsu/root/MainActivity.kt
@@ -0,0 +1,9 @@
+package app.junsu.root
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    
+}
