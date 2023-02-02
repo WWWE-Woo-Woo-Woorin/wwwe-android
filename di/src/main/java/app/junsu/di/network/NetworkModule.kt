d7884b32f99d87b7ac6280cb79a58287b5ef175a
junsuPark <krpjs0508@gmail.com>
2023-02-02 19:53:56 +0900

    ✨ :: implement NetworkModule

---
+++ di/src/main/java/app/junsu/di/network/NetworkModule.kt
@@ -0,0 +1,48 @@
+package app.junsu.di.network
+
+import android.util.Log
+import app.junsu.di.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor { log ->
+            Log.i("Http Log", log)
+        }.setLevel(
+            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+            else HttpLoggingInterceptor.Level.NONE,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient().newBuilder().addNetworkInterceptor(
+            httpLoggingInterceptor,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+    ): Retrofit {
+        return Retrofit.Builder().client(okHttpClient).baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+}
