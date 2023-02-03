69931f222e4436b7be13bbfeecc207ca486f753a
junsuPark <krpjs0508@gmail.com>
2023-02-03 15:19:40 +0900

    ✅ :: add test

--- app/src/androidTest/java/app/junsu/wwwe/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/junsu/wwwe/ExampleInstrumentedTest.kt
@@ -1,8 +1,13 @@
 package app.junsu.wwwe
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import app.junsu.domain.repository.auth.AuthRepository
+import app.junsu.domain.usecase.auth.SignUpUseCase
+import app.junsu.remote.model.auth.signup.SignUpRequest
+import app.junsu.root.MainViewModel
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -10,11 +15,24 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest @Inject constructor(
+    private val authRepository: AuthRepository,
+) {
 
     @Test
-    fun useAppContext() {
+    fun signUpTest() {
 
-        
+        val vm = MainViewModel(
+            signUpUseCase = SignUpUseCase(
+                authRepository = authRepository,
+            )
+        )
+
+        vm.signUp(
+            SignUpRequest(
+                "junsu@gmail.com",
+                "박박",
+            )
+        )
     }
 }
