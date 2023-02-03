ab3970c8a5e38a742fa4e97e9aa69e057d8d33b5
junsuPark <krpjs0508@gmail.com>
2023-02-03 19:27:54 +0900

    ✨ :: rename repository -> authRepository in SignInUseCase

--- domain/src/main/java/app/junsu/domain/usecase/auth/SignInUseCase.kt
+++ domain/src/main/java/app/junsu/domain/usecase/auth/SignInUseCase.kt
@@ -4,13 +4,13 @@ import app.junsu.domain.param.auth.SignInParam
 import app.junsu.domain.repository.auth.AuthRepository
 
 class SignInUseCase(
-    private val repository: AuthRepository,
+    private val authRepository: AuthRepository,
 ) {
     suspend operator fun invoke(
         param: SignInParam,
     ): Result<Unit> {
         return kotlin.runCatching {
-            repository.signIn(
+            authRepository.signIn(
                 email = param.email,
             )
         }
