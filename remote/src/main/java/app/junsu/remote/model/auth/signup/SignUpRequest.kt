bb94c16f50563e505108092bca362e0e10eff3c7
junsuPark <krpjs0508@gmail.com>
2023-02-03 15:21:37 +0900

    ✨ :: implement SignUpRequest

--- remote/src/main/java/app/junsu/remote/model/auth/signup/SignUpRequest.kt
+++ remote/src/main/java/app/junsu/remote/model/auth/signup/SignUpRequest.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class SignUpRequest(
     @SerializedName("email") val email: String,
     @SerializedName("username") val username: String,
-    @SerializedName("profileUrl") val profileUrl: String,
+    @SerializedName("profileUrl") val profileUrl: String? = null,
 )
