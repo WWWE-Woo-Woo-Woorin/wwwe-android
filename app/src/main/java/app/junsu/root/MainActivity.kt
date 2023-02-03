f8e014ac1a36f17c90d5cf671e874fce62b27e65
junsuPark <krpjs0508@gmail.com>
2023-02-03 19:38:17 +0900

    .test

--- app/src/main/java/app/junsu/root/MainActivity.kt
+++ app/src/main/java/app/junsu/root/MainActivity.kt
@@ -3,46 +3,75 @@ package app.junsu.root
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import app.junsu.remote.model.auth.signup.SignUpRequest
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+internal class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Column {
-                    NextButton(text = "Next")
-                }
+                MainScreen(
+                    onTestButtonClick = {
+                        viewModel.signUp(
+                            signUpRequest = SignUpRequest(
+                                "jun@naver.com",
+                                "박준",
+                                "www.naver.com",
+                            ),
+                        )
+                    },
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DemoScreen() {
-    NextButton(text = "HI")
+fun MainScreen(
+    onTestButtonClick: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        NextButton(
+            text = "테스트",
+            onClick = onTestButtonClick,
+        )
+    }
 }
 
 @Composable
-fun NextButton(
+internal fun NextButton(
     text: String,
+    onClick: () -> Unit,
 ) {
     Button(
-        onClick = { /*TODO*/ },
-        modifier = Modifier
-            .height(48.dp),
+        onClick = onClick,
+        modifier = Modifier.height(40.dp),
     ) {
         Text(text = text)
     }
 }
+
+/*
+internal sealed class NavigationRoutes(
+    internal val route: String,
+) {
+    object OnBoarding : NavCont
+}
+*/
