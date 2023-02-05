ada7294f3ef97bdebca201676886a29b6c117235
junsuPark <krpjs0508@gmail.com>
2023-02-05 15:36:54 +0900

    ✨ :: implement SpacerUtil.HorizontalSpacer

---
+++ common/common-compose/src/main/java/app/junsu/common_compose/util/SpacerUtil.kt
@@ -0,0 +1,16 @@
+package app.junsu.common_compose.util
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun VerticalSpacer(
+    height: Dp,
+) {
+    return Spacer(
+        modifier = Modifier.height(height),
+    )
+}
