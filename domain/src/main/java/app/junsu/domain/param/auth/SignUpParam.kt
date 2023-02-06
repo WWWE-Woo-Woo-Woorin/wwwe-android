bc17d518cfd6b03c53585c9780fb85da70ceaa1d
junsuPark <krpjs0508@gmail.com>
2023-02-06 20:43:10 +0900

    ✨ :: SignUpParam

--- domain/src/main/java/app/junsu/domain/param/auth/SignUpParam.kt
+++ domain/src/main/java/app/junsu/domain/param/auth/SignUpParam.kt
@@ -1,7 +1,7 @@
 package app.junsu.domain.param.auth
 
 data class SignUpParam(
-    val email: String,
-    val username: String,
-    val profileUrl: String? = null,
+    private val email: String,
+    private val username: String,
+    private val profileUrl: String? = null,
 )
