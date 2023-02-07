5468d2259046c30b344a059e49aa24d4400b2a73
junsuPark <krpjs0508@gmail.com>
2023-02-07 12:50:37 +0900

    ✨ :: implement GoogleSignInViewModel

--- feature/feature-google-sign-in/src/main/java/app/junsu/feature_google_sign_in/viewmodel/googlesignin/GoogleSignInViewModel.kt
+++ feature/feature-google-sign-in/src/main/java/app/junsu/feature_google_sign_in/viewmodel/googlesignin/GoogleSignInViewModel.kt
@@ -1,12 +1,32 @@
 package app.junsu.feature_google_sign_in.viewmodel.googlesignin
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.junsu.data.datasource.auth.RemoteAuthDataSource
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GoogleSignInViewModel @Inject constructor(
-
+    private val remoteAuthDataSource: RemoteAuthDataSource,
+    private val googleSignInClient: GoogleSignInClient,
 ) : ViewModel() {
 
+    internal val googleSignInIntent = googleSignInClient.signInIntent
+
+    private lateinit var account: GoogleSignInAccount
+
+    internal fun signInWithGoogle() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+
+                val gso =
+
+                    remoteAuthDataSource.signInWithGoogle()
+            }
+        }
+    }
 }
