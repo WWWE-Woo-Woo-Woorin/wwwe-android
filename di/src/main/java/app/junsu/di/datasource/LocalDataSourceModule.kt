0ae32c4eb749c8e88c2dab89183e33e54c80a8e3
junsuPark <krpjs0508@gmail.com>
2023-02-11 10:28:49 +0900

    ✨ :: add LocalDataSourceModule

---
+++ di/src/main/java/app/junsu/di/datasource/LocalDataSourceModule.kt
@@ -0,0 +1,12 @@
+package app.junsu.di.datasource
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocalDataSourceModule {
+
+
+}
