e3db40695a78671cfe3a2a8af6434c1a207381b0
junsuPark <krpjs0508@gmail.com>
2023-02-11 11:04:14 +0900

    ✨ :: add local module

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
     implementation(project(":domain"))
     implementation(project(":data"))
+    implementation(project(":local"))
     implementation(project(":remote"))
 
     implementation(Dependencies.Android.CORE_KTX)
