6b3c40bc63f3b65e729fe8340c452c4d6dfd7949
junsuPark <krpjs0508@gmail.com>
2023-02-13 20:56:06 +0900

    mvoe ic

--- navigator/src/main/java/app/junsu/navigator/navigation/NavigationBarItem.kt
+++ navigator/src/main/java/app/junsu/navigator/navigation/NavigationBarItem.kt
@@ -1,9 +1,7 @@
 package app.junsu.common_compose.navigation
 
-import androidx.compose.ui.graphics.vector.ImageVector
-
 data class NavigationBarItem(
     val title: String,
-    val icon: ImageVector,
+    val icon: Int,
     val route: String,
 )
