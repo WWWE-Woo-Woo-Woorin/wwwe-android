b3b5a1a68d24e50ac7a7b00a1ce7a263ee4cf2f1
junsuPark <krpjs0508@gmail.com>
2023-02-19 21:32:18 +0900

    ⚡️ :: implement Post urls

--- remote/src/main/java/app/junsu/remote/url/URL.kt
+++ remote/src/main/java/app/junsu/remote/url/URL.kt
@@ -4,11 +4,11 @@ internal object URL {
 
     object Auth {
 
-        internal const val SIGN_IN = "/v1/users/signin/{email}"
-        internal const val SIGN_UP = "/v1/users/signup"
-        internal const val FETCH_USER_INFORMATION = "/v1/users"
+        const val SIGN_IN = "/v1/users/signin/{email}"
+        const val SIGN_UP = "/v1/users/signup"
+        const val FETCH_USER_INFORMATION = "/v1/users"
 
-        internal const val REGENERATE_TOKEN = "/v1/users/token"
+        const val REGENERATE_TOKEN = "/v1/users/token"
 
         object Check {
             internal const val EMAIL_SIGNED_IN = "/v1/users/check/email"
@@ -16,9 +16,18 @@ internal object URL {
         }
 
         object SignUp {
-            internal const val SIGNUP_EMAIL = "/v1/users/signup/email"
-            internal const val ENTER_USERNAME = "/v1/users/signup/username"
-            internal const val ENTER_PROFILE_URL = "/v1/users/signup/profile"
+            const val SIGNUP_EMAIL = "/v1/users/signup/email"
+            const val ENTER_USERNAME = "/v1/users/signup/username"
+            const val ENTER_PROFILE_URL = "/v1/users/signup/profile"
         }
+    }
+
+    object Post {
+        const val CREATE_POST = "/v1/posts"
+        const val INQUIRE_ALL_POSTS = "/v1/posts"
+        const val INQUIRE_POSTS = "/v1/posts/{post-type}"
+        const val INQUIRE_POST = "/v1/posts/{post-id}"
+        const val UPDATE_POST = "/v1/posts/{post-id}"
+        const val DELETE_POST = "/v1/posts/{post-id}"
     }
 }
