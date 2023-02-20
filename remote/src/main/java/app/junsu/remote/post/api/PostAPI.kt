8c1d1b9eab84e709062ee64e87ae947345e4c5a3
junsuPark <krpjs0508@gmail.com>
2023-02-20 12:55:21 +0900

    ⚡️ :: implement PostAPI

---
+++ remote/src/main/java/app/junsu/remote/post/api/PostAPI.kt
@@ -0,0 +1,40 @@
+package app.junsu.remote.post.api
+
+import app.junsu.domain.post.model.CreatePostRequest
+import app.junsu.domain.post.model.UpdatePostRequest
+import app.junsu.model.post.PostType
+import app.junsu.remote._common.URL
+import app.junsu.remote.post.model.PostResponse
+import retrofit2.http.*
+
+interface PostAPI {
+
+    @POST(URL.Post.CREATE_POST)
+    suspend fun createPost(
+        request: CreatePostRequest,
+    ): PostResponse
+
+    @GET(URL.Post.INQUIRE_ALL_POSTS)
+    suspend fun inquireAllPosts(): List<PostResponse>
+
+    @GET(URL.Post.INQUIRE_POSTS)
+    suspend fun inquirePosts(
+        @Query("type") postType: PostType,
+    ): List<PostResponse>
+
+    @GET(URL.Post.INQUIRE_POST)
+    suspend fun inquirePost(
+        @Path("post-id") postId: Long,
+    ): PostResponse
+
+    @PATCH(URL.Post.UPDATE_POST)
+    suspend fun updatePost(
+        @Path("post-id") postId: Long,
+        @Body request: UpdatePostRequest,
+    ): PostResponse
+
+    @DELETE(URL.Post.DELETE_POST)
+    suspend fun deletePost(
+        @Path("post-id") postId: Long,
+    )
+}
