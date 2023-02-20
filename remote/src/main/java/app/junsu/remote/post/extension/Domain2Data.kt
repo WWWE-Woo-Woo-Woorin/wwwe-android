d2f7e3aa339e5a697620026e96aa9cc87bd6427d
junsuPark <krpjs0508@gmail.com>
2023-02-20 13:23:21 +0900

    ⚡️ :: implement UpdatePostRequest.toData()

--- remote/src/main/java/app/junsu/remote/post/extension/Domain2Data.kt
+++ remote/src/main/java/app/junsu/remote/post/extension/Domain2Data.kt
@@ -1,6 +1,8 @@
 package app.junsu.remote.post.extension
 
 import app.junsu.domain.post.model.CreatePostRequest as DomainCreatePostRequest
+import app.junsu.domain.post.model.UpdatePostRequest as DataUpdatePostRequest
+import app.junsu.domain.post.model.UpdatePostRequest as DomainUpdatePostRequest
 import app.junsu.remote.post.model.CreatePostRequest as DataCreatePostRequest
 
 internal fun DomainCreatePostRequest.toData(): DataCreatePostRequest {
@@ -8,5 +10,11 @@ internal fun DomainCreatePostRequest.toData(): DataCreatePostRequest {
         postImageUrl = this.postImageUrl,
         content = this.content,
         postType = this.postType,
+    )
+}
+
+internal fun DomainUpdatePostRequest.toData(): DataUpdatePostRequest {
+    return DataUpdatePostRequest(
+        content = this.content,
     )
 }
