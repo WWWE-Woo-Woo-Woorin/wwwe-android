b0ffea762397b28678e5ca0901b570f85968728f
junsuPark <krpjs0508@gmail.com>
2023-02-20 13:31:14 +0900

    ⚡️ :: implement binding PostRepository in di

--- di/src/main/java/app/junsu/di/repository/RepositoryModule.kt
+++ di/src/main/java/app/junsu/di/repository/RepositoryModule.kt
@@ -2,8 +2,10 @@ package app.junsu.di.repository
 
 import app.junsu.data.auth.repository.local.LocalLocalAuthRepositoryImpl
 import app.junsu.data.auth.repository.remote.RemoteAuthRepositoryImpl
+import app.junsu.data.post.repository.PostRepositoryImpl
 import app.junsu.domain.auth.repository.LocalAuthRepository
 import app.junsu.domain.auth.repository.remote.RemoteAuthRepository
+import app.junsu.domain.post.repository.PostRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,4 +27,10 @@ private abstract class RepositoryModule {
     abstract fun bindsRemoteAuthRepository(
         remoteAuthRepositoryImpl: RemoteAuthRepositoryImpl,
     ): RemoteAuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsPostRepository(
+        postRepositoryImpl: PostRepositoryImpl,
+    ): PostRepository
 }
