3f8ea6e5246514ac069721a22d804cc756f90803
junsuPark <krpjs0508@gmail.com>
2023-02-20 13:31:27 +0900

    ⚡️ :: implement binding PostDataSource in di

--- di/src/main/java/app/junsu/di/datasource/RemoteDataSourceModule.kt
+++ di/src/main/java/app/junsu/di/datasource/RemoteDataSourceModule.kt
@@ -1,7 +1,9 @@
 package app.junsu.di.datasource
 
 import app.junsu.data.auth.datasource.RemoteAuthDataSource
+import app.junsu.data.post.datasource.PostDataSource
 import app.junsu.remote.auth.datasource.RemoteAuthDataSourceImpl
+import app.junsu.remote.post.datasource.PostDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ private abstract class RemoteDataSourceModule {
     abstract fun bindsRemoteAuthDataSource(
         remoteAuthDataSourceImpl: RemoteAuthDataSourceImpl,
     ): RemoteAuthDataSource
+
+    @Singleton
+    @Binds
+    abstract fun bindsPostDataSource(
+        postDataSourceImpl: PostDataSourceImpl,
+    ): PostDataSource
 }
