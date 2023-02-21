1df16f3ccaa64e9b044acc0d05319c16567b1c2b
junsuPark <krpjs0508@gmail.com>
2023-02-21 16:53:12 +0900

    ⚡️ :: implement MainNavigationScreen

---
+++ feature/feature-main-nav/src/main/java/app/junsu/feature_main_nav/screen/MainNavigationScreen.kt
@@ -0,0 +1,34 @@
+package app.junsu.feature_main_nav.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import app.junsu.feature_main_nav._navigation.WWWENavigationBar
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainNavigationScreen(
+    navigationBarNavController: NavHostController,
+    content: @Composable () -> Unit,
+) {
+    Scaffold(
+        content = { paddingValues ->
+            Box(
+                modifier = Modifier.padding(paddingValues),
+            ) {
+                content()
+            }
+        },
+        bottomBar = {
+            WWWENavigationBar(
+                navController = navigationBarNavController,
+            )
+        },
+    )
+}
