ae1a3ed6914a46f2d410f4b236de45e0b3bae644
junsuPark <krpjs0508@gmail.com>
2023-04-15 22:14:09 +0900

    .

--- app/src/main/java/app/junsu/wwwe/ui/home/community/Post.kt
+++ app/src/main/java/app/junsu/wwwe/ui/home/community/Post.kt
@@ -3,5 +3,15 @@ package app.junsu.wwwe.ui.home.community
 import androidx.compose.runtime.Composable
 
 @Composable
-fun Post() {
+fun Post(
+    postId: Long,
+    imageUrl: String,
+    profileUrl: String,
+    title: String,
+    content: String,
+    totalLikes: Int,
+    liked: Boolean,
+) {
+
+
 }
