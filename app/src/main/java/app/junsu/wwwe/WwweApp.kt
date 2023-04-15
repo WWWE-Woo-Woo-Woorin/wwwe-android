eb09a817e95e2934dda5f7cfa6c86b5a544df96f
junsuPark <krpjs0508@gmail.com>
2023-04-15 20:43:22 +0900

    .

---
+++ app/src/main/java/app/junsu/wwwe/WwweApp.kt
@@ -0,0 +1,9 @@
+package app.junsu.wwwe
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun WwweApp() {
+    val navController = rememberNavController()
+}
