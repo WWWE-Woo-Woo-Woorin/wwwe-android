2d8a915e11326a836239f96492d1d254ecd64c52
junsuPark <krpjs0508@gmail.com>
2023-04-27 14:39:40 +0900

    .

--- app/src/main/java/app/junsu/wwwe/ui/WwweApp.kt
+++ app/src/main/java/app/junsu/wwwe/ui/WwweApp.kt
@@ -6,11 +6,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
-import app.junsu.wwwe.ui.home.HomeSections
 import app.junsu.wwwe.ui.home.addHomeGraph
 import app.junsu.wwwe.ui.theme.WwweTheme
 
@@ -20,7 +20,6 @@ fun WwweApp() {
         val navController = rememberNavController()
         val startDestination = WwweDestinations.HOME
 
-        println("HIHI")
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background,
@@ -29,18 +28,24 @@ fun WwweApp() {
                 navController = navController,
                 startDestination = startDestination,
             ) {
-                wwweNavGraph()
+                wwweNavGraph(
+                    navController = navController,
+                )
             }
         }
     }
 }
 
-fun NavGraphBuilder.wwweNavGraph() {
+fun NavGraphBuilder.wwweNavGraph(
+    navController: NavHostController,
+) {
     navigation(
         route = WwweDestinations.HOME,
         startDestination = HomeSections.COMMUNITY.route,
     ) {
-        addHomeGraph()
+        addHomeGraph(
+            navController = navController,
+        )
     }
     composable(
         route = WwweDestinations.AUTH,
