59536a0fc42ad1ca79cbdc01ae616287621f4641
junsuPark <krpjs0508@gmail.com>
2023-05-19 15:36:37 +0900

    refactor post

--- app/src/main/java/app/junsu/wwwe/ui/home/community/Post.kt
+++ app/src/main/java/app/junsu/wwwe/ui/home/community/Post.kt
@@ -3,12 +3,15 @@ package app.junsu.wwwe.ui.home.community
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
@@ -42,22 +45,35 @@ fun Post(
             ),
         )
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
         ) {
             GlideImage(
                 imageModel = { profileUrl },
-                modifier = Modifier.size(24.dp),
+                modifier = Modifier
+                    .padding(
+                        start = 12.dp,
+                        top = 8.dp,
+                    )
+                    .size(32.dp)
+                    .clip(CircleShape),
                 imageOptions = ImageOptions(
                     alignment = Alignment.Center,
                     contentScale = ContentScale.Crop,
                 ),
             )
             Column(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .padding(
+                        horizontal = 12.dp,
+                        vertical = 8.dp,
+                    )
+                    .weight(1f),
             ) {
                 Text(
                     text = writer,
-                    style = MaterialTheme.typography.headlineMedium,
+                    style = MaterialTheme.typography.bodyLarge,
                 )
                 Text(
                     text = content,
