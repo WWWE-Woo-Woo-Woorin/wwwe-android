2cf0f58b5e3f3e265b2162aef2aec854350c39c6
junsuPark <krpjs0508@gmail.com>
2023-05-19 15:45:08 +0900

    make scrollable

--- app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
+++ app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
@@ -1,19 +1,19 @@
 package app.junsu.wwwe.ui.home.community
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Community() {
     Column(
         Modifier
             .fillMaxSize()
-            .background(
-                color = Color.Cyan,
+            .verticalScroll(
+                state = rememberScrollState(),
             ),
     ) {}
 }
