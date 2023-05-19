ef902a60a342ddb24206c1f62a3ab7fbb56f9ead
junsuPark <krpjs0508@gmail.com>
2023-05-19 15:11:32 +0900

    http request helper

--- app/src/main/java/app/junsu/wwwe/data/HttpRequestHelper.kt
+++ app/src/main/java/app/junsu/wwwe/data/HttpRequestHelper.kt
@@ -2,9 +2,16 @@ package app.junsu.wwwe.data
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 
 class HttpRequestHelper {
-    private val client: HttpClient = HttpClient(CIO)
-
-
+    private val client: HttpClient = HttpClient(CIO) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+    }
 }
