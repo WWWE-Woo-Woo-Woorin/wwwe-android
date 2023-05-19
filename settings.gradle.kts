18fff0efba28c659329a0135817513b0659f4391
junsuPark <krpjs0508@gmail.com>
2023-05-19 15:27:25 +0900

    gradle migration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         google()
@@ -13,4 +15,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "wwwe"
-include ':app'
+include(":app")
