d919ecfc34b581b229f8adb036de2bb396348873
junsuPark <krpjs0508@gmail.com>
2023-05-20 11:01:55 +0900

    di

--- app/src/main/java/app/junsu/wwwe/WwweApplication.kt
+++ app/src/main/java/app/junsu/wwwe/WwweApplication.kt
@@ -2,13 +2,16 @@ package app.junsu.wwwe
 
 import android.app.Application
 import app.junsu.wwwe.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class WwweApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
+            androidLogger()
+            androidContext(this@WwweApplication)
             modules(
                 appModule,
             )
