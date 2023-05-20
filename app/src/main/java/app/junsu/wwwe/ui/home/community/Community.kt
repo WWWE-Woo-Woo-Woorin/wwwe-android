04d262eaa8d3dde9ac312a89273d9982d2d86fa4
junsuPark <krpjs0508@gmail.com>
2023-05-20 11:02:10 +0900

    dummy

--- app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
+++ app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
@@ -6,14 +6,29 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun Community() {
+    val viewModel :CommunityViewModel by viewModel()
+
     Column(
         Modifier
             .fillMaxSize()
             .verticalScroll(
                 state = rememberScrollState(),
             ),
-    ) {}
+    ) {
+        repeat(3) {
+            Post(
+                postId = 123,
+                imageUrl = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA0MjhfMzEg%2FMDAxNjgyNjQ4OTY0NTc5._LfcSxCiUwPlljgw6-XP2JD3PkuYkuXcqQs_WQz1Mmkg.sulvdNT6G3BZOz8fEcu_rIbBAwcwgMpWihqagmk6iJ4g.JPEG.aboard13389%2FKakaoTalk_20230420_120739846_03_%25282%2529.jpg&type=sc960_832",
+                profileUrl = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzA0MjhfMzEg%2FMDAxNjgyNjQ4OTY0NTc5._LfcSxCiUwPlljgw6-XP2JD3PkuYkuXcqQs_WQz1Mmkg.sulvdNT6G3BZOz8fEcu_rIbBAwcwgMpWihqagmk6iJ4g.JPEG.aboard13389%2FKakaoTalk_20230420_120739846_03_%25282%2529.jpg&type=sc960_832",
+                writer = "Junsu",
+                content = "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello",
+                totalLikes = 123,
+                liked = true,
+            )
+        }
+    }
 }
