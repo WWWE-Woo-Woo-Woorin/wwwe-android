a6e65e067ce579de2901d0c5cbbb9ce5c322c1ec
junsuPark <krpjs0508@gmail.com>
2023-05-20 11:01:38 +0900

    vm module

--- app/src/main/java/app/junsu/wwwe/di/AppModule.kt
+++ app/src/main/java/app/junsu/wwwe/di/AppModule.kt
@@ -1,7 +1,14 @@
 package app.junsu.wwwe.di
 
+import app.junsu.wwwe.data.CommunityRepository
+import app.junsu.wwwe.data.CommunityRepositoryImpl
+import app.junsu.wwwe.ui.home.community.CommunityViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule = module {
+    single<CommunityRepository> { CommunityRepositoryImpl() }
 
+    viewModel { CommunityViewModel(get()) }
 }
