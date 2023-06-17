0efb46bb78f41f2ed05a3a0fa8eb5014bd45e904
junsuPark <krpjs0508@gmail.com>
2023-06-17 20:24:25 +0900

    module

--- app/src/main/java/app/junsu/wwwe/di/AppModule.kt
+++ app/src/main/java/app/junsu/wwwe/di/AppModule.kt
@@ -1,16 +1,8 @@
 package app.junsu.wwwe.di
 
-import app.junsu.wwwe.data.CommunityRepository
-import app.junsu.wwwe.data.defaultHttpClient
-import app.junsu.wwwe.ui.home.community.CommunityViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import app.junsu.wwwe.data.remote.client.httpClient
 import org.koin.dsl.module
 
 val appModule = module {
-    single { defaultHttpClient }
-    single { CommunityRepository(get()) }
-
-    viewModel { CommunityViewModel(get()) }
-    viewModelOf(::CommunityViewModel)
+    single { httpClient }
 }
---
+++ app/src/main/java/app/junsu/wwwe/di/CommunityModule.kt
@@ -0,0 +1,14 @@
+package app.junsu.wwwe.di
+
+import app.junsu.wwwe.data.CommunityRepository
+import app.junsu.wwwe.ui.home.community.CommunityViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
+
+val communityModule = module {
+    single { CommunityRepository(get()) }
+
+    viewModel { CommunityViewModel(get()) }
+    viewModelOf(::CommunityViewModel)
+}
