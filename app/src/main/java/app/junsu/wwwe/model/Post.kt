3ce2d13d573f63fb9df5786a3dff30c52413f142
junsuPark <krpjs0508@gmail.com>
2023-06-17 16:55:38 +0900

    post

--- app/src/main/java/app/junsu/wwwe/model/Post.kt
+++ app/src/main/java/app/junsu/wwwe/model/Post.kt
@@ -2,7 +2,6 @@ package app.junsu.wwwe.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import java.time.LocalDateTime
 
 @Serializable
 data class Post(
@@ -10,5 +9,5 @@ data class Post(
     @SerialName("writer") val writer: String,
     @SerialName("content") val content: String,
     @SerialName("post_image_url") val postImageUrl: String,
-    //@SerialName("created_at") val createdAt: LocalDateTime,
+    @SerialName("created_at") val createdAt: String,
 )
