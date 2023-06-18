218d5a3c0bbb9bf1e0d43a2202d714dbbbe04227
junsuPark <krpjs0508@gmail.com>
2023-06-18 15:22:02 +0900

    community fab

--- app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
+++ app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
@@ -1,9 +1,15 @@
 package app.junsu.wwwe.ui.home.community
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddComment
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -13,8 +19,10 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import app.junsu.wwwe.R
 import app.junsu.wwwe.model.PostType
 import org.koin.androidx.compose.getViewModel
@@ -42,49 +50,65 @@ fun Community(
     val state = viewModel.flow.collectAsState()
     val (selectedTab, setSelectedTab) = remember { mutableStateOf(0) }
 
-    Column(
+    Box(
         modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd,
     ) {
-        TabRow(
-            selectedTabIndex = selectedTab,
-        ) {
-            CommunityTab.values().forEachIndexed { index, tab ->
-                Tab(
-                    selected = selectedTab == index,
-                    onClick = { setSelectedTab(index) },
-                    text = {
-                        Text(
-                            text = tab.text,
-                            style = MaterialTheme.typography.labelMedium,
-                            color = MaterialTheme.colorScheme.primary,
-                        )
+        Column {
+            TabRow(
+                selectedTabIndex = selectedTab,
+            ) {
+                CommunityTab.values().forEachIndexed { index, tab ->
+                    Tab(
+                        selected = selectedTab == index,
+                        onClick = { setSelectedTab(index) },
+                        text = {
+                            Text(
+                                text = tab.text,
+                                style = MaterialTheme.typography.labelMedium,
+                                color = MaterialTheme.colorScheme.primary,
+                            )
+                        },
+                    )
+                }
+            }
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+            ) {
+                val posts = state.value.posts
+
+                items(
+                    when (selectedTab) {
+                        0 -> posts
+                        1 -> posts.filter { it.postType == PostType.MAJOR }
+                        2 -> posts.filter { it.postType == PostType.CLUB }
+                        else -> throw IllegalStateException()
                     },
-                )
+                ) { post ->
+                    Post(
+                        postId = post.id,
+                        imageUrl = post.postImageUrl,
+                        profileUrl = "",
+                        writer = post.writer,
+                        content = post.content,
+                        totalLikes = 0,
+                        liked = false,
+                    )
+                }
             }
         }
-        LazyColumn(
-            modifier = Modifier.weight(1f),
-        ) {
-            val posts = state.value.posts
 
-            items(
-                when (selectedTab) {
-                    0 -> posts
-                    1 -> posts.filter { it.postType == PostType.MAJOR }
-                    2 -> posts.filter { it.postType == PostType.CLUB }
-                    else -> throw IllegalStateException()
-                },
-            ) { post ->
-                Post(
-                    postId = post.id,
-                    imageUrl = post.postImageUrl,
-                    profileUrl = "",
-                    writer = post.writer,
-                    content = post.content,
-                    totalLikes = 0,
-                    liked = false,
-                )
-            }
+        FloatingActionButton(
+            modifier = Modifier.padding(
+                bottom = 16.dp,
+                end = 16.dp,
+            ),
+            onClick = { /*TODO*/ },
+        ) {
+            Icon(
+                imageVector = Icons.Filled.AddComment,
+                contentDescription = null,
+            )
         }
     }
 }
