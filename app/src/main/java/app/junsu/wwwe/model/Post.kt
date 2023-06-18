4cca6a41063a5a29886fc4c73711fe3a8ffeab9d
junsuPark <krpjs0508@gmail.com>
2023-06-18 14:47:47 +0900

    post

--- app/src/main/java/app/junsu/wwwe/model/Post.kt
+++ app/src/main/java/app/junsu/wwwe/model/Post.kt
@@ -6,8 +6,21 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Post(
     @SerialName("post_id") val id: Long,
+    @SerialName("post_type") val postType: PostType,
     @SerialName("writer") val writer: String,
     @SerialName("content") val content: String,
     @SerialName("post_image_url") val postImageUrl: String,
     @SerialName("created_at") val createdAt: String,
 )
+
+@Serializable
+enum class PostType {
+    @SerialName("ALL")
+    DEFAULT,
+
+    @SerialName("MAJOR")
+    MAJOR,
+
+    @SerialName("CLUB")
+    CLUB, ;
+}
--- app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
+++ app/src/main/java/app/junsu/wwwe/ui/home/community/Community.kt
@@ -1,32 +1,71 @@
 package app.junsu.wwwe.ui.home.community
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import app.junsu.wwwe.model.PostType
 import org.koin.androidx.compose.getViewModel
+
+@Stable
+val communityTabs = listOf<String>("All", "Major", "Club")
 
 @Composable
 fun Community(
     viewModel: CommunityViewModel = getViewModel(),
 ) {
     val state = viewModel.flow.collectAsState()
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize(),
+    val (selectedTab, setSelectedTab) = remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
     ) {
-        items(state.value.posts) { post ->
-            Post(
-                postId = post.id,
-                imageUrl = post.postImageUrl,
-                profileUrl = "",
-                writer = post.writer,
-                content = post.content,
-                totalLikes = 0,
-                liked = false,
-            )
+        TabRow(
+            selectedTabIndex = selectedTab,
+        ) {
+            communityTabs.forEachIndexed { index, tab ->
+                Tab(selected = selectedTab == index, onClick = { setSelectedTab(index) }, text = {
+                    Text(
+                        text = tab,
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+                })
+            }
+        }
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+        ) {
+            val posts = state.value.posts
+
+            items(
+                when (selectedTab) {
+                    0 -> posts
+                    1 -> posts.filter { it.postType == PostType.MAJOR }
+                    2 -> posts.filter { it.postType == PostType.CLUB }
+                    else -> throw IllegalStateException()
+                },
+            ) { post ->
+                Post(
+                    postId = post.id,
+                    imageUrl = post.postImageUrl,
+                    profileUrl = "",
+                    writer = post.writer,
+                    content = post.content,
+                    totalLikes = 0,
+                    liked = false,
+                )
+            }
         }
     }
 }
