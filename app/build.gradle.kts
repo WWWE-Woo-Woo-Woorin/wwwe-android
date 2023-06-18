fcba0290c9ff905f37f68be7450fefc7af04e269
junsuPark <krpjs0508@gmail.com>
2023-06-18 16:26:43 +0900

    fb dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +50,8 @@ val koin_version = "3.4.0"
 val ktor_version = "2.3.0"
 
 dependencies {
+    // firebase
+    implementation("com.google.firebase:firebase-auth-ktx:22.0.0")
 
     // koin
     implementation("io.insert-koin:koin-android:$koin_version")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
 plugins {
     id("com.android.application") version "7.4.2" apply false
     id("com.android.library") version "7.4.2" apply false
