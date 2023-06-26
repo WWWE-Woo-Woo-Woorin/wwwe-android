c56cac0c1533f6ef81bc9d9a0955b7d156458f90
junsuPark <krpjs0508@gmail.com>
2023-06-26 23:20:16 +0900

    app bar 구현

---
+++ app/src/main/java/app/junsu/wwwe/ui/component/appBar.kt
@@ -0,0 +1,58 @@
+package app.junsu.wwwe.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppBar(
+    title: String? = null,
+    onNavigateUp: (() -> Unit)? = null,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(elevation = 10.dp)
+            .background(MaterialTheme.colorScheme.background)
+            .padding(
+                horizontal = 16.dp,
+                vertical = 12.dp,
+            ),
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 12.dp,
+            alignment = Alignment.Start,
+        ),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        if (onNavigateUp != null) Icon(
+            modifier = Modifier
+                .size(32.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .clickable(onClick = onNavigateUp),
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "back",
+            tint = MaterialTheme.colorScheme.onBackground,
+        )
+        if (title != null) Text(
+            modifier = Modifier.weight(1f),
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+        )
+    }
+}
