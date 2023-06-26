2d38a1dc8e010f425099103906109b069139b05a
junsuPark <krpjs0508@gmail.com>
2023-06-26 22:51:44 +0900

    create post screen

--- app/src/main/java/app/junsu/wwwe/ui/main/createpost/CreatePostScreen.kt
+++ app/src/main/java/app/junsu/wwwe/ui/main/createpost/CreatePostScreen.kt
@@ -1,21 +1,57 @@
 package app.junsu.wwwe.ui.main.createpost
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import app.junsu.wwwe.R
+import com.skydoves.landscapist.InternalLandscapistApi
+import com.skydoves.landscapist.glide.GlideImage
+import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
+@InternalLandscapistApi
 @Composable
 fun CreatePostScreen(
     modifier: Modifier = Modifier,
+    viewModel: CreatePostViewModel = koinViewModel(),
 ) {
+    val state by viewModel.flow.collectAsState()
     val (text, onTextChange) = remember { mutableStateOf("") }
+    val screenWidthDp = LocalConfiguration.current.screenWidthDp.dp
+    val pickPhotoLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { if (it != null) viewModel.updateImage(it) },
+    )
     Column(
         modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        TopAppBar(title = { stringResource(R.string.compose_new_post) })
+        GlideImage(
+            modifier = Modifier
+                .size(screenWidthDp)
+                .clickable {
+                    pickPhotoLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                },
+            imageModel = { state.selectedImageUri ?: R.drawable.img_toss },
+        )
         OutlinedTextField(
             value = text,
             onValueChange = onTextChange,
