6cc7d78c473b984e8346aab27632be14c61f8013
junsuPark <krpjs0508@gmail.com>
2023-06-27 21:28:12 +0900

    create post button

--- app/src/main/java/app/junsu/wwwe/ui/auth/signin/SignInScreen.kt
+++ app/src/main/java/app/junsu/wwwe/ui/auth/signin/SignInScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -88,6 +89,7 @@ fun SignInScreen(
                     imageVector = Icons.Filled.Add,
                     contentDescription = null,
                 )
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(
                     text = stringResource(R.string.sign_in_google),
                     style = MaterialTheme.typography.bodyMedium,
--- app/src/main/java/app/junsu/wwwe/ui/main/createpost/CreatePostScreen.kt
+++ app/src/main/java/app/junsu/wwwe/ui/main/createpost/CreatePostScreen.kt
@@ -4,10 +4,23 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddComment
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -46,6 +59,7 @@ fun CreatePostScreen(
             title = stringResource(R.string.compose_new_post),
             onNavigateUp = onNavigateUp,
         )
+        Spacer(modifier = Modifier.height(32.dp))
         GlideImage(
             modifier = Modifier
                 .size(screenWidthDp)
@@ -55,8 +69,39 @@ fun CreatePostScreen(
             imageModel = { state.selectedImageUri ?: R.drawable.img_add_photo },
         )
         OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 16.dp,
+                    vertical = 12.dp,
+                ),
             value = text,
             onValueChange = onTextChange,
+            maxLines = 4,
         )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    bottom = 8.dp,
+                    end = 16.dp,
+                ),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Button(
+                onClick = {},
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.AddComment,
+                    contentDescription = null,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = stringResource(R.string.compose),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
+        }
     }
 }
