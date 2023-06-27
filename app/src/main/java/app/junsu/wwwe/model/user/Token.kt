b9960948df65c4abff0bb88572388a6e7ac5c971
junsuPark <krpjs0508@gmail.com>
2023-06-27 22:33:46 +0900

    token

---
+++ app/src/main/java/app/junsu/wwwe/model/user/Token.kt
@@ -0,0 +1,11 @@
+package app.junsu.wwwe.model.user
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Token(
+    @SerialName("access_token") val accessToken: String,
+    @SerialName("access_token_exp") val accessTokenExp: String,
+    @SerialName("refresh_token_exp") val refreshTokenExp: String,
+)
