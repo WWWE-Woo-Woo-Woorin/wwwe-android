27e627a116dd93890f3037d7ea7d3cec7baafa34
junsuPark <krpjs0508@gmail.com>
2023-06-28 07:46:37 +0900

    data store key

--- app/src/main/java/app/junsu/wwwe/data/local/DataStore.kt
+++ app/src/main/java/app/junsu/wwwe/data/local/DataStore.kt
@@ -3,6 +3,11 @@ package app.junsu.wwwe.data.local
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "wwwe")
+
+val ACCESS_TOKEN = stringPreferencesKey("access_token")
+val ACCESS_TOKEN_EXP = stringPreferencesKey("access_token_exp")
+val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
