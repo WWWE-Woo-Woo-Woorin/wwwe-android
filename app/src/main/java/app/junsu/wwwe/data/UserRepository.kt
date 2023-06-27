1b94a3d9a95dfecf9522587c3aa5cd75ab3f6cfa
junsuPark <krpjs0508@gmail.com>
2023-06-28 07:47:05 +0900

    repository

--- app/src/main/java/app/junsu/wwwe/data/UserRepository.kt
+++ app/src/main/java/app/junsu/wwwe/data/UserRepository.kt
@@ -1,5 +1,11 @@
 package app.junsu.wwwe.data
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import app.junsu.wwwe.data.local.ACCESS_TOKEN
+import app.junsu.wwwe.data.local.ACCESS_TOKEN_EXP
+import app.junsu.wwwe.data.local.REFRESH_TOKEN
 import app.junsu.wwwe.data.remote.BASE_URL
 import app.junsu.wwwe.model.user.SignInRequest
 import app.junsu.wwwe.model.user.Token
@@ -12,11 +18,29 @@ import io.ktor.http.contentType
 
 class UserRepository(
     private val httpClient: HttpClient,
+    private val dataStore: DataStore<Preferences>,
 ) {
     suspend fun signIn(request: SignInRequest): Token {
         return httpClient.post("$BASE_URL/v1/users/signin") {
             contentType(ContentType.Application.Json)
             setBody(request)
         }.body()
+    }
+
+    suspend fun saveTokens(token: Token) {
+        dataStore.edit {
+            it[ACCESS_TOKEN] = token.accessToken
+            it[ACCESS_TOKEN_EXP] = token.accessTokenExp
+            it[REFRESH_TOKEN] = token.refreshToken
+        }
+    }
+
+    /**
+     * @return [Result] whether sign in success
+     */
+    suspend fun signInWithSavingToken(request: SignInRequest) {
+        val token = this.signIn(request)
+        this.saveTokens(token)
+        println("TOKENTOKEN $token")
     }
 }
