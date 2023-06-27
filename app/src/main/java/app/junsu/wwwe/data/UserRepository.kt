8f5c5a78c220f163a7b3a02bc07298b35f5f02a3
junsuPark <krpjs0508@gmail.com>
2023-06-28 08:41:57 +0900

    token methods

--- app/src/main/java/app/junsu/wwwe/data/UserRepository.kt
+++ app/src/main/java/app/junsu/wwwe/data/UserRepository.kt
@@ -5,6 +5,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import app.junsu.wwwe.data.local.ACCESS_TOKEN
 import app.junsu.wwwe.data.local.ACCESS_TOKEN_EXP
+import app.junsu.wwwe.data.local.EMAIL
 import app.junsu.wwwe.data.local.REFRESH_TOKEN
 import app.junsu.wwwe.data.remote.BASE_URL
 import app.junsu.wwwe.model.user.SignInRequest
@@ -12,9 +13,12 @@ import app.junsu.wwwe.model.user.Token
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import java.io.IOException
+import kotlinx.coroutines.flow.first
 
 class UserRepository(
     private val httpClient: HttpClient,
@@ -27,7 +31,14 @@ class UserRepository(
         }.body()
     }
 
-    suspend fun saveTokens(token: Token) {
+    suspend fun regenerateToken() {
+        val email = this.findEmail() ?: throw IOException()
+        httpClient.put("$BASE_URL/v1/users/token") {
+            setBody(SignInRequest(email = email))
+        }
+    }
+
+    suspend fun saveToken(token: Token) {
         dataStore.edit {
             it[ACCESS_TOKEN] = token.accessToken
             it[ACCESS_TOKEN_EXP] = token.accessTokenExp
@@ -35,12 +46,46 @@ class UserRepository(
         }
     }
 
+    suspend fun findToken(): Token? {
+        val accessToken = this.findAccessToken() ?: return null
+        val accessTokenExp = this.findAccessTokenExp() ?: return null
+        val refreshToken = this.findRefreshToken() ?: return null
+
+        return Token(
+            accessToken = accessToken,
+            accessTokenExp = accessTokenExp,
+            refreshToken = refreshToken,
+        )
+    }
+
+    suspend fun saveEmail(email: String) {
+        dataStore.edit {
+            it[EMAIL] = email
+        }
+    }
+
+    suspend fun findEmail(): String? {
+        return dataStore.data.first()[EMAIL]
+    }
+
+    suspend fun findAccessToken(): String? {
+        return dataStore.data.first()[ACCESS_TOKEN]
+    }
+
+    suspend fun findAccessTokenExp(): String? {
+        return dataStore.data.first()[ACCESS_TOKEN_EXP]
+    }
+
+    suspend fun findRefreshToken(): String? {
+        return dataStore.data.first()[REFRESH_TOKEN]
+    }
+
     /**
      * @return [Result] whether sign in success
      */
-    suspend fun signInWithSavingToken(request: SignInRequest) {
+    suspend fun signInWithSavingTokenAndEmail(request: SignInRequest) {
         val token = this.signIn(request)
-        this.saveTokens(token)
-        println("TOKENTOKEN $token")
+        this.saveToken(token)
+        this.saveEmail(request.email)
     }
 }
