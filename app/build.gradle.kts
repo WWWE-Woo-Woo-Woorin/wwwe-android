1a2bfb7f39383bc77f7a916fba2720db9ab58bfb
junsuPark <krpjs0508@gmail.com>
2023-06-28 23:30:15 +0900

    remove test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,11 +86,6 @@ dependencies {
     // stream chat SDK
     implementation("io.getstream:stream-chat-android-compose:5.12.0")
 
-    // test
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     // kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
