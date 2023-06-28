e8b0b106340fc72c675f08420c4753e25d6b248c
junsuPark <krpjs0508@gmail.com>
2023-06-29 08:06:06 +0900

    vm

--- app/src/main/kotlin/app/junsu/wwwe/MainViewModel.kt
+++ app/src/main/kotlin/app/junsu/wwwe/MainViewModel.kt
@@ -2,38 +2,17 @@ package app.junsu.wwwe
 
 import androidx.lifecycle.ViewModel
 import app.junsu.wwwe.data.UserRepository
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.runBlocking
 
 class MainViewModel(
     private val userRepository: UserRepository,
 ) : ViewModel() {
-    val sideEffectFlow = MutableStateFlow<MainSideEffect?>(null)
-
     fun regenerateToken(): Boolean {
         return runBlocking {
             val result = kotlin.runCatching {
                 userRepository.regenerateToken()
-            }.onFailure {
-                it.printStackTrace()
-            }
-
-            val success = result.isSuccess
-
-            sideEffectFlow.tryEmit(
-                if (success) {
-                    MainSideEffect.TokenAvailable
-                } else {
-                    MainSideEffect.TokenNotAvailable
-                },
-            )
-
-            return@runBlocking success
+            }.onFailure { it.printStackTrace() }
+            return@runBlocking result.isSuccess
         }
     }
-}
-
-sealed interface MainSideEffect {
-    object TokenAvailable : MainSideEffect
-    object TokenNotAvailable : MainSideEffect
 }
