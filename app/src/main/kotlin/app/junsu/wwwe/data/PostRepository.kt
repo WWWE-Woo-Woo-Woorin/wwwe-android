0af33c3c75fec22f435e75591d5384fa86020755
junsuPark <krpjs0508@gmail.com>
2023-06-29 13:53:35 +0900

    post image

--- app/src/main/kotlin/app/junsu/wwwe/data/PostRepository.kt
+++ app/src/main/kotlin/app/junsu/wwwe/data/PostRepository.kt
@@ -1,8 +1,10 @@
 package app.junsu.wwwe.data
 
+import android.net.Uri
 import app.junsu.wwwe.data.remote.BASE_URL
 import app.junsu.wwwe.model.post.ComposePostRequest
 import app.junsu.wwwe.model.post.Post
+import com.google.firebase.storage.FirebaseStorage
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
@@ -28,5 +30,14 @@ class PostRepository(
         return httpClient.get("$BASE_URL/v1/posts") {
             bearerAuth(tokenFacade.findAccessTokenOrRegenerate())
         }.body()
+    }
+
+    fun uploadFile(fileUri: Uri): String {
+        val dbRef = FirebaseStorage.getInstance().reference
+        val fileRef =
+            dbRef.child("${System.currentTimeMillis()}.${fileUri.toString().split(".")[1]}")
+        fileRef.putFile(fileUri).addOnSuccessListener {}
+
+        return "stub"
     }
 }
