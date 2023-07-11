4b972735e22374668de9db11d52d4139c531781b
junsuPark <krpjs0508@gmail.com>
2023-07-11 11:03:36 +0900

    post

--- app/src/main/kotlin/app/junsu/wwwe/data/PostRepository.kt
+++ app/src/main/kotlin/app/junsu/wwwe/data/PostRepository.kt
@@ -5,7 +5,8 @@ import app.junsu.wwwe.data.remote.BASE_URL
 import app.junsu.wwwe.model.post.ComposePostRequest
 import app.junsu.wwwe.model.post.Post
 import app.junsu.wwwe.model.post.PostType
-import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.bearerAuth
@@ -28,27 +29,30 @@ class PostRepository(
         }.body()
     }
 
-    fun composePost(
+    suspend fun composePost(
         postImageUri: Uri,
         content: String,
         postType: PostType,
     ) {
-        val dbRef = FirebaseStorage.getInstance().reference
-        val fileRef = dbRef.child(
-            "${System.currentTimeMillis()}.${postImageUri.toString().split(".")[1]}",
-        )
-        fileRef.putFile(postImageUri)
-        fileRef.downloadUrl.addOnSuccessListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                val request = ComposePostRequest(
-                    postImageUrl = it.toString(),
-                    content = content,
-                    postType = postType.name,
-                )
-                httpClient.post("$BASE_URL/v1/posts") {
-                    contentType(ContentType.Application.Json)
-                    bearerAuth(tokenFacade.findAccessTokenOrRegenerate())
-                    setBody(request)
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val fileRef = storageRef.child("${System.currentTimeMillis()}.png")
+        val task = fileRef.putFile(postImageUri)
+        task.addOnProgressListener {
+            println("PROGRESS ${it.bytesTransferred}/${it.totalByteCount}")
+        }.addOnSuccessListener {
+            fileRef.downloadUrl.addOnSuccessListener { uri ->
+                CoroutineScope(Dispatchers.IO).launch {
+                    val request = ComposePostRequest(
+                        postImageUrl = uri.toString(),
+                        content = content,
+                        postType = postType.name,
+                    )
+                    httpClient.post("$BASE_URL/v1/posts") {
+                        contentType(ContentType.Application.Json)
+                        bearerAuth(tokenFacade.findAccessTokenOrRegenerate())
+                        setBody(request)
+                    }
                 }
             }
         }
